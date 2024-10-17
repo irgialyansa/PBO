2dc1205cbed37bcd9d5b118d7b0dbd3366bf6411
irgialyansa <155567535+irgialyansa@users.noreply.github.com>
2024-10-18 01:05:21 +0700

    Add files via upload

---
+++ JavaCasting.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaCasting;
+
+/**
+ *
+ * @author orgyy
+ */
+public class JavaCasting {
+    
+    public static void main(String[] args) {
+        
+        // Nama : Irgi Alyansa Perdana
+        //kelas : K23B
+        //casting = mengubah bentuk tipe data
+        int myInt = 78;
+        double myDouble = myInt;
+        //cetak
+//        System.out.println("Nilai Integer :" + myInt);
+//        System.out.println("Nilai Double :" + myDouble);
+        
+        //Doble ke Int
+        double nilai1 = 26.0d;
+        int nilai2 = (int) nilai1;
+        
+        //cetak
+        System.out.println("Nilai double :" + nilai1);
+        System.out.println("Nilai Integer :" + nilai2);
+        
+    }
+}
---
+++ JavaComment.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaComment;
+
+/**
+ *
+ * @author orgyy
+ */
+public class JavaComment {
+    //komentar 1
+    public static void main(String[] args) {
+        // Saya sedang membuat komentar
+        System.out.println("Irgi komentar");
+        
+    //komentar 2
+    /*
+    komentar irgi 1
+    komentar irgi 2
+    komentar irgi 3
+    
+    */
+    }
+    
+}
